602423e1f03ef6d3cb43b774aa3071fd9a0b61d6
Adam DeHaven <adam.dehaven@konghq.com>
2024-03-14 14:30:35 -0400

    chore: do not use prod config

--- layers/base/nuxt.config.ts
+++ layers/base/nuxt.config.ts
@@ -9,13 +9,8 @@ export default defineNuxtConfig({
     prerender: {
       autoSubfolderIndex: false // Set to false to match Cloudflare route matching rules: https://nuxt.com/deploy/cloudflare
     },
-  },
-  $production: {
-    nitro: {
-      // !Important: we only want to enable the wasm feature in production since it will break syntax highlighting when running the dev server
-      experimental: {
-        wasm: true,
-      },
+    experimental: {
+      wasm: true,
     },
   },
   vite: {
